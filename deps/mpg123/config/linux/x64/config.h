72660dbf34beb2245c6a9637117ea83111a65948
Neico <admin@neic0.de>
2016-08-15 06:58:17 +0200

    as I only need / have linux x64 I can't fix the other config.h files...

--- deps/mpg123/config/linux/x64/config.h
+++ deps/mpg123/config/linux/x64/config.h
@@ -11,6 +11,12 @@
 /* Define if building universal (internal helper macro) */
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
 
+/* Define if .balign is present. */
+#define ASMALIGN_BALIGN 1
+
+/* Define if .align just takes byte count. */
+/* #undef ASMALIGN_BYTE */
+
 /* Define if .align takes 3 for alignment of 2^3=8 bytes instead of 8. */
 /* #undef ASMALIGN_EXP */
 
@@ -25,6 +31,9 @@
 
 /* Define if building with dynamcally linked libmpg123 */
 /* #undef DYNAMIC_BUILD */
+
+/* Use EFBIG as substitude for EOVERFLOW, mingw.org may lack the latter */
+/* #undef EOVERFLOW */
 
 /* Define if FIFO support is enabled. */
 /* #undef FIFO */
@@ -76,6 +85,9 @@
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
+
+/* Define if getaddrinfo accepts the AI_ADDRCONFIG flag */
+#define HAVE_GAI_ADDRCONFIG 1
 
 /* Define to 1 if you have the `getaddrinfo' function. */
 #define HAVE_GETADDRINFO 1
@@ -179,9 +191,6 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
-/* Define to 1 if you have the `strdup' function. */
-#define HAVE_STRDUP 1
-
 /* Define to 1 if you have the `strerror' function. */
 #define HAVE_STRERROR 1
 
@@ -208,6 +217,9 @@
 
 /* Define to 1 if you have the <sys/resource.h> header file. */
 #define HAVE_SYS_RESOURCE_H 1
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
 
 /* Define to 1 if you have the <sys/signal.h> header file. */
 #define HAVE_SYS_SIGNAL_H 1
@@ -251,12 +263,11 @@
 /* Define if IPV6 support is enabled. */
 #define IPV6 1
 
-/* Define this to the size of long type in bits, used for LFS small/native
-   alias functions. */
+/* Define this to the size of native offset type in bits, used for LFS alias
+   functions. */
 #define LFS_ALIAS_BITS 64
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
 #define LT_OBJDIR ".libs/"
 
 /* The suffix for module files. */
@@ -276,6 +287,9 @@
 
 /* Define to disable downsampled decoding. */
 /* #undef NO_DOWNSAMPLE */
+
+/* Define to disable equalizer. */
+/* #undef NO_EQUALIZER */
 
 /* Define to disable error messages in combination with a return value (the
    return is left intact). */
@@ -302,6 +316,9 @@
 /* Define to disable layer III. */
 /* #undef NO_LAYER3 */
 
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
 /* Define to disable ntom resampling. */
 /* #undef NO_NTOM */
 
@@ -311,6 +328,9 @@
 /* Define to disable string functions. */
 /* #undef NO_STRING */
 
+/* Define for post-processed 32 bit formats. */
+/* #undef NO_SYNTH32 */
+
 /* Define to disable warning messages. */
 /* #undef NO_WARNING */
 
@@ -318,13 +338,13 @@
 #define PACKAGE "mpg123"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "mpg123-devel@lists.sourceforge.net"
+#define PACKAGE_BUGREPORT "maintainer@mpg123.org"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "mpg123"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "mpg123 1.14.4"
+#define PACKAGE_STRING "mpg123 1.23.6"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "mpg123"
@@ -333,7 +353,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.14.4"
+#define PACKAGE_VERSION "1.23.6"
 
 /* Define if portaudio v18 API is wanted. */
 /* #undef PORTAUDIO18 */
@@ -359,8 +379,14 @@
 /* Define if modules are enabled */
 /* #undef USE_MODULES */
 
+/* Define for new Huffman decoding scheme. */
+#define USE_NEW_HUFFTABLE 1
+
+/* Define to use yasm for assemble AVX sources. */
+/* #undef USE_YASM_FOR_AVX */
+
 /* Version number of package */
-#define VERSION "1.14.4"
+#define VERSION "1.23.6"
 
 /* Define to use Win32 named pipes */
 /* #undef WANT_WIN32_FIFO */
@@ -386,6 +412,11 @@
 # endif
 #endif
 
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
 
@@ -409,6 +440,12 @@
 
 /* Define to `int' if <sys/types.h> does not define. */
 /* #undef int32_t */
+
+/* Define to `long long' if <sys/types.h> does not define. */
+/* #undef int64_t */
+
+/* Define to the native offset type (long or actually off_t). */
+#define lfs_alias_t off_t
 
 /* Define to `long int' if <sys/types.h> does not define. */
 /* #undef off_t */
